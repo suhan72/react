2627251823e523e0a15e98e6136f1f4c4429c086
suhan72 <100765700+suhan72@users.noreply.github.com>
2022-04-25 15:30:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { ExpenseItems } from './components/ExpenseItems'
+
+
+function App() {
+  return (
+    <>
+    <h1>let's get Started !! Yeah</h1>
+    <ExpenseItems/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const ExpenseItems = () => {
+  return (
+   <>
+   <h1> Expense Items</h1>
+
+<p>Food Rs 10</p>
+
+<p>Petrol Rs 100</p>
+
+<p>Movies Rs 200</p>
+    </>
+  )
+}
