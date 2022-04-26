5ccb71bc6ac4b13769dab42b844d5b23c45deb62
suhan72 <100765700+suhan72@users.noreply.github.com>
2022-04-26 19:01:37 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import Expenses from './components/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -28,7 +28,7 @@ const App = () => {
  
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
