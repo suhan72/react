332c9c42929c166dc415a49c407cf5acf8cd165b
suhan72 <100765700+suhan72@users.noreply.github.com>
2022-04-28 02:06:17 +0530

    Add files via upload

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
@@ -14,7 +13,9 @@ const Expenses = (props) => {
 
   const filteredExpenses = props.items.filter(expense =>{
     return expense.date.getFullYear().toString() === filteredYear;
-  })
+  });
+
+ 
 
   return (
     <div>
@@ -23,14 +24,9 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key = {expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+ 
+       <ExpensesList items={filteredExpenses}/>
+       
       </Card>
     </div>
   );
